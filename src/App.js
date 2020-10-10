bd1d616e56cafea5d39e820916d485d27988f49b
Alex Markin <alexndr86@gmail.com>
2020-10-10 13:47:54 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <h1>I am home</h1>
+    </div>
+  );
 }
 
 export default Home;
