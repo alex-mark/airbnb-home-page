b7d96e566dfd52af08454ffd0b883d60eb075390
Alex Markin <alexndr86@gmail.com>
2020-10-10 15:01:59 +0300

    Add date picker component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import "./Banner.css";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+
+        <Button
+          className="banner__searchButton"
+          variant="outlined"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          Search Dates
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import "./Search.css";
+
+function Search() {
+  return <div className="search"></div>;
+}
+
+export default Search;
