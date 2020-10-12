01aa90b125453b9b4291c5a85e383225492ca67e
Alex Markin <alexndr86@gmail.com>
2020-10-12 17:00:53 +0300

    Add SearchPage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -7,9 +8,20 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route to="/" exact>
+            <Home />
+          </Route>
+          <Route to="/search">
+            <SearchPage />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
